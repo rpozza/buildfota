3f5742d2189aae36b63fb2372c0bef1cad5d1f25
Riccardo Pozza <r.pozza@surrey.ac.uk>
2017-05-30 18:00:09 +0100

    Add run them all

---
+++ 9_compile_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./4_compile-multiple-wifi-main.sh config/zeroth-batch-unis.json
+./4_compile-multiple-wifi-main.sh config/first-batch-unis.json
+./4_compile-multiple-wifi-main.sh config/second-batch-unis.json
+./4_compile-multiple-wifi-main.sh config/third-batch-unis.json
+./4_compile-multiple-wifi-main.sh config/fourth-batch-unis.json
+./4_compile-multiple-wifi-main.sh config/fifth-batch-unis.json
+
